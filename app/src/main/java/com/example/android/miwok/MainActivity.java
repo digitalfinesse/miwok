32b27c05db7e7edccd655b3367dcfba46df4159c
Evgeniy Palguev <dizverst@gmail.com>
2017-09-13 23:59:41 +0300

    some changes

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+ 
 
         // Ищем View, показывающую категорию colors
         TextView buttonViewPhrases = (TextView) findViewById(phrases);
